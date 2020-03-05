5886cb34ee35a893a1addec7300a4830f3344878
Zhora <Zhora@i.ua>
2020-03-05 23:26:22 +0200

    some baby steps

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Redis as RedisAlias;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $visits = RedisAlias::incr('visit');
+    return view('welcome')->withVisits($visits);
 });
